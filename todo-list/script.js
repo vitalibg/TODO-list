b8e19c91ff39f03b4357504dcfee733a82ed4950
Vitali Akulau <vitalibg2023@gmail.com>
2023-10-17 09:11:51 +0300

    add completeTodoHandler

--- todo-list/script.js
+++ todo-list/script.js
@@ -205,51 +205,7 @@ function getToDo(todoText, todoData, todoId) {
   todoCloseWrap.append(todoDateContainer);
   todo.append(todoCloseWrap);
   todo.addEventListener("click", (event) => closeTodoHandler(event));
-
-  todo.addEventListener("click", (event) => {
-    if (event.target.className === "checkmark") {
-      event.target.parentElement.parentElement.parentElement.classList.toggle(
-        "completed",
-      );
-      event.target.parentElement.parentElement.parentElement.children[1].classList.toggle(
-        "cross-text",
-      );
-      event.target.parentElement.parentElement.parentElement.parentElement.parentElement.children[1].children.item(
-        1,
-      ).firstChild.textContent = `Completed: ${
-        event.target.parentElement.parentElement.parentElement.parentElement.getElementsByClassName(
-          "completed",
-        ).length
-      }`;
-
-      const completedTodos = [];
-      const todoList =
-        event.target.parentElement.parentElement.parentElement.parentElement
-          .children;
-      const todosList = JSON.parse(localStorage.getItem("todos"));
-
-      for (let i = 0; i < todoList.length; i++) {
-        if (todoList[i].className.includes("todo-wrap completed")) {
-          completedTodos.push(i);
-        }
-      }
-
-      for (let i = 0; i < todosList.length; i++) {
-        todosList[i].todoIsChecked = false;
-      }
-
-      for (let i = 0; i < todosList.length; i++) {
-        for (let j = 0; j < completedTodos.length; j++) {
-          if (i === completedTodos[j]) {
-            todosList[i].todoIsChecked = true;
-          }
-        }
-      }
-
-      localStorage.clear();
-      localStorage.setItem("todos", JSON.stringify(todosList));
-    }
-  });
+  todo.addEventListener("click", (event) => completeTodoHandler(event));
   return todo;
 }
 
@@ -345,4 +301,49 @@ function closeTodoHandler(event) {
   }
   localStorage.clear();
   localStorage.setItem("todos", JSON.stringify(todosList));
+}
+
+function completeTodoHandler(event) {
+  if (event.target.className === "checkmark") {
+    event.target.parentElement.parentElement.parentElement.classList.toggle(
+        "completed",
+    );
+    event.target.parentElement.parentElement.parentElement.children[1].classList.toggle(
+        "cross-text",
+    );
+    event.target.parentElement.parentElement.parentElement.parentElement.parentElement.children[1].children.item(
+        1,
+    ).firstChild.textContent = `Completed: ${
+        event.target.parentElement.parentElement.parentElement.parentElement.getElementsByClassName(
+            "completed",
+        ).length
+    }`;
+
+    const completedTodos = [];
+    const todoList =
+        event.target.parentElement.parentElement.parentElement.parentElement
+            .children;
+    const todosList = JSON.parse(localStorage.getItem("todos"));
+
+    for (let i = 0; i < todoList.length; i++) {
+      if (todoList[i].className.includes("todo-wrap completed")) {
+        completedTodos.push(i);
+      }
+    }
+
+    for (let i = 0; i < todosList.length; i++) {
+      todosList[i].todoIsChecked = false;
+    }
+
+    for (let i = 0; i < todosList.length; i++) {
+      for (let j = 0; j < completedTodos.length; j++) {
+        if (i === completedTodos[j]) {
+          todosList[i].todoIsChecked = true;
+        }
+      }
+    }
+
+    localStorage.clear();
+    localStorage.setItem("todos", JSON.stringify(todosList));
+  }
 }
